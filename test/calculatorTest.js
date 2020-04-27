689a743587cb0b685d32e5a500b3c8bb29c895c3
sapna <sapnapatil344@gmail.com>
2020-04-27 08:38:12 +0530

    MOCKING: stubbing on add method

--- test/calculatorTest.js
+++ test/calculatorTest.js
@@ -5,29 +5,29 @@ let addition = require('../modules/addition');
 let calculation = require('../modules/calculator');
 
 describe('calculator Test', () => {
-    
+
     it('it should calculate 2 nos', (done) => {
         let add = sinon.stub(addition, "add")
-        add.withArgs( 1, 4 ).resolves(5);
-        calculation.cal(1, 4).then(function(result){
-            expect(result).to .equals(5);
+        add.withArgs(1, 4).resolves(5);
+        calculation.cal(1, 4).then(function (result) {
+            expect(result).to.equals(5);
             done();
-        }).catch(function(err){
+        }).catch(function (err) {
             done(err);
         })
         add.restore();
     })
 
-    // it('Add twice', (done) => {
-    //     let add = sinon.stub(addition, "add").onFirstCall().resolves(5).onCall(1).resolves(9)
-    //     add.withArgs(sinon.match.any, sinon.match.any)
-    //     calculation.calculate(1, 4).then(function(result){
-    //         expect(result).to .equals(9);
-    //         done();
-    //     }).catch(function(err){
-    //         done(err);
-    //     })
-    //     add.restore();
-    // })
+    it('Add twice', (done) => {
+        let add = sinon.stub(addition, "add").onFirstCall().resolves(5).onCall(1).resolves(9)
+        add.withArgs(sinon.match.any, sinon.match.any)
+        calculation.calculate(1, 4).then(function (result) {
+            expect(result).to.equals(9);
+            done();
+        }).catch(function (err) {
+            done(err);
+        })
+        add.restore();
+    })
 })
 
